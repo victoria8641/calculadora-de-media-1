da938dc9c90c29038e1741a47eaee58d8bc1808b
victoria8641 <victoria.8641@colegioprogresso.g12.br>
2026-03-04 11:22:21 -0300

    pegamos os dados

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
-alert("deu certo")
+const numero1 = document.getElementById("numero1")
+ const numero2 = document.getElementById("numero2") 
+ 
+ const button = document.getElementById("button") 
+
+ function CalcularMedia(){
+   const valorNota1 = Number()nota1.value
+   const valorNota2 = nota2.value
+
+console.log(valorNota1 + valorNota2)
+ }
+ 
+ button.addEventListener("click", calcularMedia)
+
