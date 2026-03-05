71aaaf6a250f63dedc9b897134e25928b5cf3178
victoria8641 <victoria.8641@colegioprogresso.g12.br>
2026-03-05 07:48:46 -0300

    finalizamos a média

--- script.js
+++ script.js
@@ -1,13 +1,16 @@
-const numero1 = document.getElementById("numero1")
- const numero2 = document.getElementById("numero2") 
- 
+ const nota1 = document.getElementById("numero1")
+ const nota2 = document.getElementById("numero2") 
+ const resultado = document.getElementById("resultado")
+
  const button = document.getElementById("button") 
 
  function CalcularMedia(){
-   const valorNota1 = Number()nota1.value
-   const valorNota2 = nota2.value
+   const valorNota1 = Number(nota1.value)
+   const valorNota2 = Number(nota2.value)
 
-console.log(valorNota1 + valorNota2)
+   const média = (valorNota1 + valorNota2)/2
+
+   resultado.innerText = `A média é ${media.toFixed(2)}`
  }
  
  button.addEventListener("click", calcularMedia)
